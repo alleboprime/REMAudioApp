85228cbd7b990c603711fe462be4ed2407eff41c
alleboprime <alessiobo2006@gmail.com>
2025-04-12 09:32:31 +0200

    added key enter event for submit ip and log in

--- lib/screens/log_screen.dart
+++ lib/screens/log_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:rem_app/components/logScreen/log_screen_components.dart';
 import 'package:rem_app/dimensions.dart';
@@ -18,12 +19,28 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _focusNode.requestFocus();
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
   PageController loginScreenPageController = PageController(initialPage: 0);
 
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
   final TextEditingController serverIpController = TextEditingController();
+
+  final FocusNode _focusNode = FocusNode();
 
   UserModel model = UserModel();
 
@@ -52,6 +69,12 @@ class LoginScreenState extends State<LoginScreen> {
   set failingReason(String value){
     _failingReason = value;
     failed = true;
+  }
+
+  void checkKeyPressed(KeyEvent keyPressed){
+    if (keyPressed.logicalKey == LogicalKeyboardKey.enter) {
+      model.isLogging ? login() : submitIp();
+    }
   }
 
 //TODO check if pushNamed instead of restorablePushNamed generate errors.
@@ -122,121 +145,125 @@ class LoginScreenState extends State<LoginScreen> {
     final dimensions = Dimensions();
 
     return SafeArea(
-      child: GestureDetector(
-        onTap: () {
-          FocusScope.of(context).unfocus();
-        },
-        child: Consumer<UserModel>(
-            builder: (context, model, child) {
-              WidgetsBinding.instance.addPostFrameCallback((_) {
-                if (!model.isLogging) {
-                  loginScreenPageController.animateToPage(
-                    0,
-                    duration: Duration(milliseconds: 200),
-                    curve: Curves.easeInOut,
-                  );
-                } else {
-                  loginScreenPageController.animateToPage(
-                    1,
-                    duration: Duration(milliseconds: 200),
-                    curve: Curves.easeInOut,
-                  );
+      child: KeyboardListener(
+        onKeyEvent: checkKeyPressed,
+        focusNode: _focusNode,
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Consumer<UserModel>(
+              builder: (context, model, child) {
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  if (!model.isLogging) {
+                    loginScreenPageController.animateToPage(
+                      0,
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.easeInOut,
+                    );
+                  } else {
+                    loginScreenPageController.animateToPage(
+                      1,
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.easeInOut,
+                    );
+                  }
                 }
-              }
-              );
-
-              return Stack(
-                children: [
-                  Scaffold(
-                    resizeToAvoidBottomInset: true,
-                    backgroundColor: Colors.black,
-                    body: Center(
-                      child: Column(
-                        children: [
-                          Expanded(
-                            flex: dimensions.extremeNarrow ? 0 : 2,
-                            child: dimensions.extremeNarrow
-                              ? SizedBox.shrink()
-                              : Container(
-                                padding: EdgeInsets.only(top: 60, left: 50, right: 50),
-                                alignment: Alignment.center,
-                                child: SvgPicture.asset(
-                                  colorFilter: ColorFilter.mode(
-                                    Colors.black.withAlpha(40),
-                                    BlendMode.srcATop,
-                                  ),
-                                  "assets/rem_logo.svg"
-                                )
-                              )
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: SizedBox(
-                              width: dimensions.logScreenTextBoxWidht,
-                              child: PageView(
-                                physics: NeverScrollableScrollPhysics(),
-                                controller: loginScreenPageController,
-                                children: [
-                                  ServerIpPage(serverIpController: serverIpController,),
-                                  LoginPage(usernameController: usernameController, passwordController: passwordController,),
-                                ]
-                              ),
-                            )
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: SingleChildScrollView(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  LogScreenButton(
-                                    width_: dimensions.logScreenButtonWidht,
-                                    height_: dimensions.logScreenButtonHeight,
-                                    text: model.isLogging ? "Log In" : "Submit",
-                                    action: model.isLogging ? login : submitIp,
-                                  ),
-                                  if(model.isLogging)
-                                    SizedBox(height: 5,),
-                                  if(model.isLogging)
-                                    LogScreenText(
-                                      text: "Go Back"
+                );
+        
+                return Stack(
+                  children: [
+                    Scaffold(
+                      resizeToAvoidBottomInset: true,
+                      backgroundColor: Colors.black,
+                      body: Center(
+                        child: Column(
+                          children: [
+                            Expanded(
+                              flex: dimensions.extremeNarrow ? 0 : 2,
+                              child: dimensions.extremeNarrow
+                                ? SizedBox.shrink()
+                                : Container(
+                                  padding: EdgeInsets.only(top: 60, left: 50, right: 50),
+                                  alignment: Alignment.center,
+                                  child: SvgPicture.asset(
+                                    colorFilter: ColorFilter.mode(
+                                      Colors.black.withAlpha(40),
+                                      BlendMode.srcATop,
                                     ),
-                                ],
+                                    "assets/rem_logo.svg"
+                                  )
+                                )
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: SizedBox(
+                                width: dimensions.logScreenTextBoxWidht,
+                                child: PageView(
+                                  physics: NeverScrollableScrollPhysics(),
+                                  controller: loginScreenPageController,
+                                  children: [
+                                    ServerIpPage(serverIpController: serverIpController,),
+                                    LoginPage(usernameController: usernameController, passwordController: passwordController,),
+                                  ]
+                                ),
+                              )
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    LogScreenButton(
+                                      width_: dimensions.logScreenButtonWidht,
+                                      height_: dimensions.logScreenButtonHeight,
+                                      text: model.isLogging ? "Log In" : "Submit",
+                                      action: model.isLogging ? login : submitIp,
+                                    ),
+                                    if(model.isLogging)
+                                      SizedBox(height: 5,),
+                                    if(model.isLogging)
+                                      LogScreenText(
+                                        text: "Go Back"
+                                      ),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ),
-                  if (isLoading)
-                    Container(
-                      color: Colors.black.withAlpha(180),
-                      child: Center(
-                        child: SizedBox(
-                          width: 50,
-                          height: 50,
-                          child: CircularProgressIndicator(
-                            strokeWidth: 5,
+                          ],
+                        ),
+                      )
+                    ),
+                    if (isLoading)
+                      Container(
+                        color: Colors.black.withAlpha(180),
+                        child: Center(
+                          child: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: CircularProgressIndicator(
+                              strokeWidth: 5,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  if (failed)
-                    Positioned(
-                      bottom: 20,
-                      left: 20,
-                      right: 20,
-                      child: ShadToast(
-                        backgroundColor: colors.logScreenToastColor,
-                        description: Text(failingReason, style: TextStyle(fontSize: dimensions.isPc ? 17 : 14),),
+                    if (failed)
+                      Positioned(
+                        bottom: 20,
+                        left: 20,
+                        right: 20,
+                        child: ShadToast(
+                          backgroundColor: colors.logScreenToastColor,
+                          description: Text(failingReason, style: TextStyle(fontSize: dimensions.isPc ? 17 : 14),),
+                        ),
                       ),
-                    ),
-                ]
-              );
-              },
-          )
-        )
+                  ]
+                );
+                },
+            )
+          ),
+      )
     );
   }
 }
